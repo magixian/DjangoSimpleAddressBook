0422524808f48e3f3c63bc693d61a4a2045d0b85
Enock Mberego <enock.mberego@gmail.com>
2018-04-10 10:35:58 +0200

    Changes to allow contacts to be edited and deleted.

--- addressbook/contacts/urls.py
+++ addressbook/contacts/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('view/<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('add/', views.add, name='add'),
+    path('edit/<int:pk>/', views.add, name='edit'),
     path('save/', views.save, name='save'),
+    path('update/<int:pk>/', views.save, name='update'), # update url will use the function with save
+    path('delete/<int:pk>/', views.delete, name='delete'),
 ]
--- addressbook/contacts/views.py
+++ addressbook/contacts/views.py
@@ -20,18 +20,45 @@ class DetailView(generic.DetailView):
     model = Contact
     template_name = 'contacts/details.html'
 
-def add(request):
-    return render(request, 'contacts/add.html', {'form': ContactForm(),})
+def add(request, pk=None):
+    # Check to see if a key has been sent. If true then allow edit functionality.
+    if pk == None:
+        form = ContactForm()
+    else:
+        contact = get_object_or_404(Contact, pk=pk)
+        form = ContactForm(request.POST or None, instance=contact)
 
-def save(request):
-    #contact = get_object_or_404(Contact, pk=pk)
+    return render(request, 'contacts/add.html', {'form': form,})
+
+def add(request, pk=None):
+    # Check to see if a key has been sent. If true then allow edit functionality.
+    if pk == None:
+        return render(request, 'contacts/add.html', {'form': ContactForm(),})
+    else:
+        contact = get_object_or_404(Contact, pk=pk)
+        form = ContactForm(request.POST or None, instance=contact)
+        return render(request, 'contacts/add.html', {'form': form, 'contact': contact})
+
+def save(request, pk=None):
     if request.method == "POST":
-        form = ContactForm(request.POST)
+        # If a key is sent then get the object and dupdate it.
+        if pk == None:
+            form = ContactForm(request.POST)
+        else:
+            contact = get_object_or_404(Contact, pk=pk)
+            form = ContactForm(request.POST or None, instance=contact)
+
         if form.is_valid():
             contact = form.save(commit=False)
             contact.save()
             return HttpResponseRedirect(reverse('contacts:detail', args=(contact.id,)))
         else:
-            return render(request, 'contacts/add.html', {'form': form, 'error_message': "Pleas enter all the contact information required.",})
+            return render(request, 'contacts/add.html', {'form': form, 'error_message': "Please enter all the contact information required.",})
     else:
         return render(request, 'contacts/add.html', {'form': ContactForm(), 'error_message': "Please re-enter the contact.",})
+
+def delete(request, pk):
+    # Check to see if a key has been sent then delete
+    contact = get_object_or_404(Contact, pk=pk)
+    contact.delete()
+    return HttpResponseRedirect(reverse('contacts:index'))
